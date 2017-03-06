801c2a9af5d63feb75f19d2bd042b718d347371a
ismail basha <ismail basha>
2017-03-05 20:10:05 -0600

    movie class

---
+++ src/MovieDataExcercise/ModelClass1.java
@@ -0,0 +1,70 @@
+package MovieDataExcercise;
+import java.util.Date;
+
+/*You are given following Data points:
+		Userid
+		Movieid
+		Rating
+		timestamp
+		Model  a class based on above parameters
+		*/
+public class ModelClass1 {
+
+	
+			private int Userid;
+			private int  movieid;
+			private double rating;
+			private Date timestamp;
+
+			public void setUserId(int Userid) {
+				this.Userid = Userid;
+			}
+
+			public int getUserId() {
+				return Userid;
+			}
+
+			public void setMovieid(int movieid) {
+				this.movieid = movieid;
+			}
+
+			public int getMovieid() {
+				return movieid;
+			}
+
+			public void setRating(double rating) {
+				this.rating = rating;
+			}
+
+			public double getRating() {
+				return rating;
+			}
+
+			public void setTimestamp(Date timestamp) {
+				this.timestamp = timestamp;
+			}
+
+			public Date getTimestamp() {
+				return timestamp;
+			}
+
+			public static void main(String[] args) {
+				ModelClass m = new ModelClass();
+				m.setUserId(111);
+				m.setMovieid(1);
+				m.setRating(3);
+				m.getTimestamp();
+
+				System.out.println(m.getUserId());
+				System.out.println(m.getMovieid());
+				System.out.println(m.getRating());
+				System.out.println(m.getTimestamp());
+
+			}
+
+		
+
+	
+
+}
+
