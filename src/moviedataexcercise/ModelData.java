8f94a38385df21008201bb0e994b28165bba641c
arya990 <arya990@gmail.com>
2017-03-16 09:52:01 -0500

    movie data excercise

--- src/moviedataexcercise/ModelData.java
+++ src/moviedataexcercise/ModelData.java
@@ -1,5 +1,6 @@
 package moviedataexcercise;
 
+
 /*You are given following Data points:
 		Userid
 		Movieid
@@ -7,20 +8,36 @@ package moviedataexcercise;
 		timestamp
 		Model  a class based on above parameters
 		*/
-public class ModelData {
+public class ModelData implements Comparable<ModelData> {
 
-	private int Userid;
+	private int userid;
 	private int movieid;
 	private double rating;
 	private double junk;
 	private String timestamp;
 
-	public void setUserId(int Userid) {
-		this.Userid = Userid;
+	public ModelData() {
+		userid = 0;
+		movieid = 0;
+		rating = 0.0;
+		junk = 0.0;
+		timestamp = " ";
+	}
+
+	public ModelData(int userid, int movieid, double rating, double junk, String timestamp) {
+		this.userid = userid;
+		this.rating = rating;
+		this.movieid = movieid;
+		this.junk = junk;
+		this.timestamp = timestamp;
+	}
+
+	public void setUserId(int userid) {
+		this.userid = userid;
 	}
 
 	public int getUserId() {
-		return Userid;
+		return userid;
 	}
 
 	public void setMovieid(int movieid) {
@@ -54,4 +71,14 @@ public class ModelData {
 	public String getTimestamp() {
 		return timestamp;
 	}
+
+	public int compareTo(ModelData a) {
+		if (rating == a.rating)
+			return 0;
+		else if (rating > a.rating)
+			return -1;
+		else  
+			return 1;
+	}
+	
 }
--- src/moviedataexcercise/MovieDataClass.java
+++ src/moviedataexcercise/MovieDataClass.java
@@ -1,23 +1,45 @@
 package moviedataexcercise;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.MalformedJsonException;
 
 public class MovieDataClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MalformedJsonException {
 
 		ArrayList<ModelData> datas = new ArrayList<ModelData>();
-		Set<Integer> list = new HashSet<Integer>();
 		Set<Integer> list1 = new HashSet<Integer>();
-		ArrayList<Double> list2 = new ArrayList<Double>();
+		Set<Integer> list2 = new HashSet<Integer>();
+		ArrayList<Integer> list3 = new ArrayList<Integer>();
+		ArrayList<Integer> list4 = new ArrayList<Integer>();
+
+		Map<Integer, Integer> map1 = new HashMap<>();
+
+		Double max_rating = (double) Float.MIN_VALUE;
+		Double min_rating = (double) Float.MAX_VALUE;
+		int value = 0;
 
 		try {
-			BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\basha\\Desktop\\Vote.txt"));
+			FileReader file = new FileReader("C:\\Users\\basha\\Desktop\\Vote.txt");
+			BufferedReader br = new BufferedReader(file);
 
 			String line = " ";
 			while ((line = br.readLine()) != null) {
@@ -36,16 +58,107 @@ public class MovieDataClass {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
 		for (ModelData a : datas) {
-			list.add(a.getUserId());
-			list1.add(a.getMovieid());
+			list1.add(a.getUserId());
+			list2.add(a.getMovieid());
 			// System.out.println(a.getUserId());
+			if (a.getRating() > max_rating) {
+				max_rating = a.getRating();
+			}
+			if (a.getRating() < min_rating) {
+				min_rating = a.getRating();
+			}
+		}
+		for (ModelData m : datas) {
+			if (max_rating == m.getRating()) {
+				list3.add(m.getMovieid());
+			} else if (m.getRating() == 0.0) {
+				list4.add(m.getMovieid());
+			}
+		}
+
+		for (ModelData m : datas) {
+			if (!map1.containsKey(m.getUserId())) {
+				map1.put(m.getUserId(), 1);
+				value = 0;
+			} else {
+				value = value + 1;
+				map1.put(m.getUserId(), value);
+			}
 
 		}
 
-		System.out.println("number of unique users:" + list.size());
-		System.out.println("number of unique movies:" + list1.size());
+		Set<Entry<Integer, Integer>> set = map1.entrySet();
+		List<Entry<Integer, Integer>> list5 = new ArrayList<Entry<Integer, Integer>>(set);
+		Collections.sort(list5, new Comparator<Map.Entry<Integer, Integer>>() {
+			@Override
+			public int compare(Map.Entry<Integer, Integer> o1, Entry<Integer, Integer> o2) {
+				return (o2.getValue()).compareTo(o1.getValue());
+			}
+		});
 
+		List<Entry<Integer, Integer>> list6 = new ArrayList<Entry<Integer, Integer>>(set);
+		Collections.sort(list6, new Comparator<Map.Entry<Integer, Integer>>() {
+			@Override
+			public int compare(Map.Entry<Integer, Integer> o1, Entry<Integer, Integer> o2) {
+				return (o1.getValue()).compareTo(o2.getValue());
+			}
+		});
+
+		System.out.println(max_rating);
+		System.out.println(min_rating);
+
+		System.out.println("number of unique users:" + list1.size());
+		System.out.println("number of unique movies:" + list2.size());
+
+		System.out.println("Find 10 Movies with Highest Rating");
+		for (int i = 0; i < 10; i++) {
+			System.out.print(list3.get(i) + " ");
+		}
+		System.out.println();
+		System.out.println("Find 10 Movies with Lowest Rating");
+		for (int i = 0; i < 10; i++) {
+			System.out.print(list4.get(i) + " ");
+		}
+		System.out.println();
+		System.out.println("Find 10 Users who watched most movies");
+		// System.out.println(map1);
+		for (int i = 0; i < 10; i++) {
+			System.out.print(list5.get(i) + "  ");
+		}
+		System.out.println();
+		System.out.println("Find 10 Users who watched less movies");
+		for (int i = 0; i < 10; i++) {
+			System.out.print(list6.get(i) + "  ");
+		}
+		txtToJson();
 	}
 
+	public static void txtToJson() {
+
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\basha\\Desktop\\Vote.txt"));
+
+			String line = " ";
+			while ((line = br.readLine()) != null) {
+				JsonObject jsonObject = new JsonObject();
+
+				try {
+					JsonParser parser = new JsonParser();
+					JsonElement jsonElement = parser.parse(new FileReader("C:\\Users\\basha\\Desktop\\Vote.txt"));
+					jsonObject = jsonElement.getAsJsonObject();
+					System.out.println(jsonElement);
+				} catch (FileNotFoundException e) {
+					throw new JsonSyntaxException(e);
+				} catch (IOException e) {
+					throw new JsonIOException(e);
+				} catch (NumberFormatException e) {
+					throw new JsonSyntaxException(e);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
