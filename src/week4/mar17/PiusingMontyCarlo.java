7fa0bd191ff49ece04dcf2e4790984e97a0c2a60
arya990 <arya990@gmail.com>
2017-03-18 18:17:02 -0500

    pi using monte carlo method

---
+++ src/week4/mar17/PiusingMontyCarlo.java
@@ -0,0 +1,22 @@
+package week4.mar17;
+
+public class PiusingMontyCarlo {
+
+	public static void main(String[] args) {
+		double x, y;
+		int hits = 0;
+		// int limit = Integer.MAX_VALUE;
+
+		for (int i = 0; i < 1000; i++) {
+			x = Math.random();
+			y = Math.random();
+			if (x * x + y * y <= 1)
+				hits++;
+		}
+
+		System.out.println(4.0 * hits / 1000);
+		System.out.println(Math.PI);
+
+	}
+
+}
