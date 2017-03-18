195d939a54d532145b05e07ea65425a1620a76c9
arya990 <arya990@gmail.com>
2017-03-18 18:16:09 -0500

    cpu and ram usage using java program

---
+++ src/week4/mar17/CpuRamandSystemUsage.java
@@ -0,0 +1,36 @@
+package week4.mar17;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//Cpu,Ram,System Usage commands in java
+
+public class CpuRamandSystemUsage extends WindowsSystemInformation{
+
+	public static void main(String[] args) throws IOException {
+		WindowsSystemInformation information= new WindowsSystemInformation();
+		information.get();
+		System.out.println(information.get());
+	}
+}
+class WindowsSystemInformation
+{
+    String get() throws IOException
+    {
+        Runtime runtime = Runtime.getRuntime();
+        Process process = runtime.exec("systeminfo");
+        BufferedReader systemInformationReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+        StringBuilder stringBuilder = new StringBuilder();
+        String line;
+
+        while ((line = systemInformationReader.readLine()) != null)
+        {
+            stringBuilder.append(line);
+            stringBuilder.append(System.lineSeparator());
+        }
+
+        return stringBuilder.toString().trim();
+    }
+}
