8077341ca65aa1d28802dac9b5ae7fef9da79660
arya990 <arya990@gmail.com>
2017-03-19 23:18:45 -0500

    pi using montey carlo

--- src/week4/mar17/PiusingMontyCarlo.java
+++ src/week4/mar17/PiusingMontyCarlo.java
@@ -1,5 +1,5 @@
 package week4.mar17;
-
+//finding pi value using montey carlo 
 public class PiusingMontyCarlo {
 
 	public static void main(String[] args) {
@@ -7,14 +7,14 @@ public class PiusingMontyCarlo {
 		int hits = 0;
 		// int limit = Integer.MAX_VALUE;
 
-		for (int i = 0; i < 1000; i++) {
+		for (int i = 0; i < 100000; i++) {
 			x = Math.random();
 			y = Math.random();
 			if (x * x + y * y <= 1)
 				hits++;
 		}
 
-		System.out.println(4.0 * hits / 1000);
+		System.out.println(4.0 * hits / 100000);
 		System.out.println(Math.PI);
 
 	}
