20cf23ba843711bbf154acb92a6e87736f700158
arya990 <arya990@gmail.com>
2017-03-19 23:20:03 -0500

    cpu ram disk usage

---
+++ src/week4/mar17/CpuRamDiskUsage1.java
@@ -0,0 +1,31 @@
+package week4.mar17;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+//Cpu,Ram,System Usage commands in java
+
+public class CpuRamDiskUsage1 {
+	public static void main(String[] args) {
+		CpuRamDiskUsage1 cpuRamDiskUsage1 = new CpuRamDiskUsage1();
+		cpuRamDiskUsage1.printUsage();
+
+	}
+
+	private void printUsage() {
+		OperatingSystemMXBean operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
+		for (Method method : operatingSystemMXBean.getClass().getDeclaredMethods()) {
+			method.setAccessible(true);
+			if (method.getName().startsWith("get") && Modifier.isPublic(method.getModifiers())) {
+				Object value;
+				try {
+					value = method.invoke(operatingSystemMXBean);
+				} catch (Exception e) {
+					value = e;
+				} // try
+				System.out.println(method.getName() + " = " + value);
+			} // if
+		} // for
+	}
+}
