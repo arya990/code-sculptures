7a1e2ad4e9a5c07602e6722ef5a8e53cc7f081f2
arya990 <arya990@gmail.com>
2017-03-30 00:12:58 -0500

    converting json file to csv

---
+++ src/readingdifferentfileformats/GotClassFile.java
@@ -0,0 +1,87 @@
+package readingdifferentfileformats;
+
+public class GotClassFile {
+	
+	private String id;
+	private String url;
+	private String name;
+	private String season;
+	private String number;
+	private String airdate;
+	private String airstamp;
+	private String airtime;
+	private String runtime;
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	public String getUrl() {
+		return url;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setSeason(String season) {
+		this.season = season;
+	}
+	
+	public String getSeason() {
+		return season;
+	}
+	
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	
+	public String getNumber() {
+		return number;
+	}
+	
+	public void setAirdate(String airdate) {
+		this.airdate = airdate;
+	}
+	
+	public String getAirdate() {
+		return airdate;
+	}
+	
+	public void setAirstamp(String airstamp) {
+		this.airstamp = airstamp;
+	}
+	
+	public String getAirstamp() {
+		return airstamp;
+	}
+	
+	public void setRuntime(String runtime) {
+		this.runtime = runtime;
+	}
+	
+	public String getRuntime() {
+		return runtime;
+	}
+	
+	public void setAirtime(String airtime) {
+		this.airtime = airtime;
+	}
+	
+	public String getAirtime() {
+		return airtime;
+	}
+
+}
