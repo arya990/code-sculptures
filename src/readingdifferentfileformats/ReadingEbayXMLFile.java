de08908a82e5e71011125ebae1db210a8939fbbd
arya990 <arya990@gmail.com>
2017-04-15 20:04:57 -0500

    reading an ebay xml file

--- src/readingdifferentfileformats/ReadingEbayXMLFile.java
+++ src/readingdifferentfileformats/ReadingEbayXMLFile.java
@@ -1,15 +1,23 @@
 package readingdifferentfileformats;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.NodeList;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
-//Read XML file, and print to console
+//Read ebay.xml file, and print to console
 
 public class ReadingEbayXMLFile {
+	public static final String delimiter = ",";
+	public static final String new_line = "\n";
+	public static final String Header = "seller_name,description";
 
 	public static void main(String[] args) {
 
@@ -19,15 +27,41 @@ public class ReadingEbayXMLFile {
 
 			DocumentBuilder builder = builderFactory.newDocumentBuilder();
 
-			org.w3c.dom.Document document = builder.parse(new File("E:\\workspace\\textfile\\ebay.xml"));
+			Document document = builder.parse(new File("E:\\workspace\\textfile\\ebay.xml"));
 
-			org.w3c.dom.Element element = (document).getDocumentElement();
+			List<EbayClass> list = new ArrayList<EbayClass>();
 
-			NodeList nodes = ((Node) element).getChildNodes();
+			NodeList nodes = document.getDocumentElement().getChildNodes();
 
 			for (int i = 0; i < nodes.getLength(); i++) {
-				System.out.println("" + nodes.item(i).getTextContent());
+				Node node = nodes.item(i);
+
+				if (node.getNodeType() == Node.ELEMENT_NODE) {
+					Element element = (Element) node;
+
+					String s = element.getElementsByTagName("seller_name").item(0).getTextContent();
+					String d = element.getElementsByTagName("description").item(0).getChildNodes().item(0)
+							.getNodeValue();
+					EbayClass class1 = new EbayClass(s, d);
+					list.add(class1);
+				}
 			}
+
+			FileWriter writer = new FileWriter(new File("E:\\workspace\\textfile\\ebay.csv"));
+
+			writer.append(Header);
+			// writer.append(delimiter);
+			writer.append(new_line);
+
+			for (EbayClass class1 : list) {
+				writer.append("\"" + class1.getSeller_name() + "\"");
+				writer.append(delimiter);
+				writer.append("\"" + class1.getDescription() + "\"");
+				writer.append(new_line);
+
+			}
+
+			writer.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
